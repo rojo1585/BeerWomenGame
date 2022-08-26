0f0b9941dd9f0535af10e6200acb795fd3c235bd
Rojo1585 <jorge_mendoza1585@hotmail.com>
2022-08-26 00:39:07 -0500

    AttackBoss

--- Scripts/SpellController.cs
+++ Scripts/SpellController.cs
@@ -7,15 +7,29 @@ public class SpellController : MonoBehaviour
 
     private Animator animator;
     private GameObject Player;
+    [SerializeField] private BoxCollider2D spellCollider;
+
+    [SerializeField] private float offsetBoxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        Player = GameObject.Find("Player");
+        spellCollider = spellCollider.GetComponent<BoxCollider2D>();
+        offsetBoxCollider = 0.02f;
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         
+            ChangeZize();
+        
+    }
+
+    private void ChangeZize(){
+        spellCollider.size = new Vector3(0.1421051f,0.4182262f);
     }
 }
