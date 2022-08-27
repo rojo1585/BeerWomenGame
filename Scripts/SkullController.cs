e3517736d3ba0db2d5d3b786c6e57573b232ff22
Rojo1585 <jorge_mendoza1585@hotmail.com>
2022-08-27 00:36:55 -0500

    Skill Prefab and script

--- Scripts/SkullController.cs
+++ Scripts/SkullController.cs
@@ -8,12 +8,15 @@ public class SkullController : MonoBehaviour
     [SerializeField] private Transform player;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+   
+    private float damage;
      // Start is called before the first frame update
     void Start()
     {
-        animator.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         
+        damage = 100;
     }
 
     // Update is called once per frame
@@ -23,6 +26,24 @@ public class SkullController : MonoBehaviour
     }
 
     public void TurnSkull(Vector3 target){
-        if(transform.position.x < target.x){spriteRenderer.flipX = true;}else{spriteRenderer.flipX = false;}
+        if(transform.position.x > target.x){spriteRenderer.flipX = true;}else{spriteRenderer.flipX = false;}
     }
+
+    private void DestroySkull(){
+        Destroy(gameObject);
+    }
+
+    private void MakeDamage(){
+        //
+       
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            animator.SetBool("Destroy",true);
+            collision.GetComponent<PlayerController>().TakeDamagePlayer(damage);
+        }
+    }
+
 }
